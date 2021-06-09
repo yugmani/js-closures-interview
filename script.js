d38dc08fce3e4f1a480078c605807811819f1186
yugmani <yugmani@yahoo.com>
2021-06-08 20:09:44 -0700

    shorthand of OR with if statement

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('hello!')
+// 1. If shorthand
+
+const favoriteFruit = fruit => {
+  // if (    // longhand
+  //   fruit === 'apple' ||
+  //   fruit === 'banana' ||
+  //   fruit === 'cherry' ||
+  //   fruit === 'mango'
+  // ) {
+  //   return 'My favorite food';
+  // }
+
+  // shorthand
+  if (['apple', 'banana', 'cherry', 'mango'].includes(fruit)) {
+    return 'My favorite food';
+  } else {
+    return 'Not a favorite';
+  }
+};
+
+// console.log(favoriteFruit('mango'));
+// Expected: My favorite food
+// console.log(favoriteFruit('berry'));
+// Expected: Not a favorite
+
