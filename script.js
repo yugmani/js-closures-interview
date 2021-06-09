140f818d2bc2ec7162801234c33540f20d67c8ff
yugmani <yugmani@yahoo.com>
2021-06-08 22:51:20 -0700

    ternary operator along with nested

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ const favoriteFruit = fruit => {
 // console.log(favoriteFruit('berry'));
 // Expected: Not a favorite
 
+// 2. Ternary Operator instead of if ... else;
+
+const myChoiceMoney = money => {
+  let myChoice = Boolean;
+  // if (money > 100) {   // Longhand
+  //   myChoice = true;
+  // } else myChoice = false;
+  // return myChoice;
+
+  //shorthand
+  // return myChoice = (money > 100) ? true : false;
+
+  // Super shorthand
+  return (myChoice = money > 100);
+};
+
+// console.log(myChoiceMoney(200));
+// console.log(myChoiceMoney(85));
+
+// 3. Nested shorthand ternary operator
+
+const littleBigGrand = size => {
+  return (result = size > 1000 ? 'Grande' : size < 100 ? 'Little' : 'Big');
+};
+
+// console.log(littleBigGrand(1100)); // Grande
+// console.log(littleBigGrand(700)); // Big
+// console.log(littleBigGrand(90)); // Little
