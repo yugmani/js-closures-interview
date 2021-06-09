9253f74a2eeb36b35de6df53b083b7eea535466b
yugmani <yugmani@yahoo.com>
2021-06-09 00:07:25 -0700

    null, undefined or empty

--- script.js
+++ script.js
@@ -51,3 +51,40 @@ const littleBigGrand = size => {
 // console.log(littleBigGrand(1100)); // Grande
 // console.log(littleBigGrand(700)); // Big
 // console.log(littleBigGrand(90)); // Little
+
+// 4. Checking not null values
+// shorthand
+// let data1;
+let data2 = data1 || 'nothing';
+console.log(data2); // expected nothing;
+
+// 5. Assigning default values
+
+// NULL
+let test1,
+  test2 = test1 ?? 0; // default 0;
+
+// console.log('test1: ' + test1); // null;
+// console.log('test2: ' + test2); // 0
+
+// UNDEFINED
+let value1 = undefined,
+  value2 = value1 || '';
+
+// console.log('value1: ' + value1); // undefined
+// console.log('value2: ' + value2); // empty
+
+// 6. Nullish Operator
+
+const user = null ?? "Yoog";
+// console.log(user); // Yoog
+
+const price = 1 ?? 4;
+// console.log(price); // 1
+
+// 6. Assigning values;
+
+let [str1, str2, str3] = ["aa", "", "cc"];
+// console.log([str1, str3]); //["aa", "cc"]
+// console.log([str2]); // [""]
+
